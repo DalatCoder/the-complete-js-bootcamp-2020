e33ff72e164d4b6acff0237ce8d9393adef6335f
DalatCoder <hieuntctk42@gmail.com>
2020-05-01 21:36:05 +0700

    Init commit 🚀

---
+++ chapter_4.Controll-flow/app.js
@@ -0,0 +1,5 @@
+const number = 19
+
+if (number % 2 !== 0) {
+  console.log('Odd number!')
+}
---
+++ chapter_8.Function-overview/app.js
@@ -0,0 +1,26 @@
+// isValidPassword function
+// // Password must:
+// - be at least 8 characters
+//   - cannot contain any spaces
+// - cannot contain username
+
+function isValidPassword(password, username) {
+  if (password.length < 8) return false;
+  if (password.includes(username)) return false;
+  if (password.includes(" ")) return false;
+
+  return true;
+}
+
+document
+  .querySelector("input[type='submit']")
+  .addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const password = document.querySelector('input[type="password"]').value;
+    const username = document.querySelector('input[type="text"]').value;
+
+    if (!isValidPassword(password, username)) {
+      alert("Invalid password!");
+    }
+  });
