cd40126d163dc163b0d1f9210279baebe36436a1
DalatCoder <hieuntctk42@gmail.com>
2020-05-03 16:41:27 +0700

    Practive sort method

--- chapter_10.ArrayMethod/array-sort.js
+++ chapter_10.ArrayMethod/array-sort.js
@@ -36,3 +36,72 @@ console.log(sort_descending);
 // Default sort: sort by ascii
 const badSort = array.slice().sort();
 console.log(badSort);
+
+const books = [
+  {
+    title: "Good Omens",
+    authors: ["Terry Pratchett", "Neil Gaiman"],
+    rating: 4.25,
+    genres: ["fiction", "fantasy"],
+  },
+  {
+    title: "Changing My Mind",
+    authors: ["Zadie Smith"],
+    rating: 3.83,
+    genres: ["nonfiction", "essays"],
+  },
+  {
+    title: "Bone: The Complete Edition",
+    authors: ["Jeff Smith"],
+    rating: 4.42,
+    genres: ["fiction", "graphic novel", "fantasy"],
+  },
+  {
+    title: "American Gods",
+    authors: ["Neil Gaiman"],
+    rating: 4.11,
+    genres: ["fiction", "fantasy"],
+  },
+  {
+    title: "A Gentleman in Moscow",
+    authors: ["Amor Towles"],
+    rating: 4.36,
+    genres: ["fiction", "historical fiction"],
+  },
+  {
+    title: "The Name of the Wind",
+    authors: ["Patrick Rothfuss"],
+    rating: 4.54,
+    genres: ["fiction", "fantasy"],
+  },
+  {
+    title: "The Overstory",
+    authors: ["Richard Powers"],
+    rating: 4.19,
+    genres: ["fiction", "short stories"],
+  },
+  {
+    title: "The Way of Kings",
+    authors: ["Brandon Sanderson"],
+    rating: 4.65,
+    genres: ["fantasy", "epic"],
+  },
+  {
+    title: "Lord of the flies",
+    authors: ["William Golding"],
+    rating: 3.67,
+    genres: ["fiction"],
+  },
+];
+
+const asc_ratingBooks = books.slice().sort((a, b) => a.rating - b.rating);
+console.log("Ascending books by rating:");
+for (const book of asc_ratingBooks) {
+  console.log(`${book.title}: ${book.rating}`);
+}
+
+const des_ratingBooks = books.slice().sort((a, b) => b.rating - a.rating);
+console.log("Descending books by rating:");
+for (const book of des_ratingBooks) {
+  console.log(`${book.title}: ${book.rating}`);
+}
