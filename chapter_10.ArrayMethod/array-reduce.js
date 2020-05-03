e8c719fa4480e73743d173dca75257b94b84525f
DalatCoder <hieuntctk42@gmail.com>
2020-05-03 19:20:16 +0700

    Use reduce to find min max of an array

--- chapter_10.ArrayMethod/array-reduce.js
+++ chapter_10.ArrayMethod/array-reduce.js
@@ -1,3 +1,15 @@
 const array = [3, 4, 5, 6, 7];
 const sum = array.reduce((total, curVal) => total + curVal);
-console.log(sum);
+console.log(`Sum: ${sum}`);
+
+const grades = [50, 89, 95, 45, 70, 66, 80, 92, 83];
+const maxGrade = grades.reduce((max, curVal) => {
+  // if (curVal > max) return curVal;
+  // return max;
+
+  return Math.max(max, curVal);
+});
+console.log(`Max: ${maxGrade}`);
+
+const minGrade = grades.reduce((min, curVal) => Math.min(min, curVal));
+console.log(`Min: ${minGrade}`);
