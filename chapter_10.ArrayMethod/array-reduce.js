c39810e543f95cc876a0d4e13fa48fc0a82f5729
DalatCoder <hieuntctk42@gmail.com>
2020-05-03 19:43:47 +0700

    Use reduce method to group array

--- chapter_10.ArrayMethod/array-reduce.js
+++ chapter_10.ArrayMethod/array-reduce.js
@@ -20,3 +20,18 @@ const total = [10, 20, 30, 40, 50].reduce((total, curVal) => {
   return total + curVal;
 }, 0);
 console.log(`Total: ${total}`);
+
+// prettier-ignore
+const votes = ["absent","y", "y", "n", "y", "n", "n", "y", "y", "y", "n", "y", "n", "y"];
+const results = votes.reduce((tally, val) => {
+  // if (tally[val]) tally[val]++;
+  // else {
+  //   tally[val] = 1;
+  // }
+
+  // tally[val] ? tally[val]++ : (tally[val] = 1);
+
+  tally[val] = (tally[val] || 0) + 1;
+  return tally;
+}, {});
+console.log(results);
