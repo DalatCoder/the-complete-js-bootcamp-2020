d29d721b7e2166803d9ac6fbd0e72a65146fb4f8
DalatCoder <hieuntctk42@gmail.com>
2020-05-24 15:23:02 +0700

    NBA Scores

--- chapter_13.DOM/NBA-scores/app.js
+++ chapter_13.DOM/NBA-scores/app.js
@@ -1,121 +1,115 @@
-const warriorsGames = [{
+const warriorsGames = [
+  {
     awayTeam: {
-      team: 'Golden State',
+      team: "Golden State",
       points: 119,
-      isWinner: true
+      isWinner: true,
     },
     homeTeam: {
-      team: 'Houston',
+      team: "Houston",
       points: 106,
-      isWinner: false
-    }
+      isWinner: false,
+    },
   },
   {
     awayTeam: {
-      team: 'Golden State',
+      team: "Golden State",
       points: 105,
-      isWinner: false
+      isWinner: false,
     },
     homeTeam: {
-      team: 'Houston',
+      team: "Houston",
       points: 127,
-      isWinner: true
-    }
+      isWinner: true,
+    },
   },
   {
     homeTeam: {
-      team: 'Golden State',
+      team: "Golden State",
       points: 126,
-      isWinner: true
+      isWinner: true,
     },
     awayTeam: {
-      team: 'Houston',
+      team: "Houston",
       points: 85,
-      isWinner: false
-    }
+      isWinner: false,
+    },
   },
   {
     homeTeam: {
-      team: 'Golden State',
+      team: "Golden State",
       points: 92,
-      isWinner: false
+      isWinner: false,
     },
     awayTeam: {
-      team: 'Houston',
+      team: "Houston",
       points: 95,
-      isWinner: true
-    }
+      isWinner: true,
+    },
   },
   {
     awayTeam: {
-      team: 'Golden State',
+      team: "Golden State",
       points: 94,
-      isWinner: false
+      isWinner: false,
     },
     homeTeam: {
-      team: 'Houston',
+      team: "Houston",
       points: 98,
-      isWinner: true
-    }
+      isWinner: true,
+    },
   },
   {
     homeTeam: {
-      team: 'Golden State',
+      team: "Golden State",
       points: 115,
-      isWinner: true
+      isWinner: true,
     },
     awayTeam: {
-      team: 'Houston',
+      team: "Houston",
       points: 86,
-      isWinner: false
-    }
+      isWinner: false,
+    },
   },
   {
     awayTeam: {
-      team: 'Golden State',
+      team: "Golden State",
       points: 101,
-      isWinner: true
+      isWinner: true,
     },
     homeTeam: {
-      team: 'Houston',
+      team: "Houston",
       points: 92,
-      isWinner: false
-    }
-  }
-]
+      isWinner: false,
+    },
+  },
+];
 
 // **************************************************
 // STEP 1 - UGLY, UN-REFACTORED CODE! (but it works!)
 // **************************************************
 
-const ulParent = document.createElement('ul');
+const ulParent = document.createElement("ul");
 for (let game of warriorsGames) {
-  const {
-    homeTeam,
-    awayTeam
-  } = game;
-  const gameLi = document.createElement('li');
-  const {
-    team: hTeam,
-    points: hPoints
-  } = homeTeam;
-  const {
-    team: aTeam,
-    points: aPoints
-  } = awayTeam;
+  const { homeTeam, awayTeam } = game;
+  const gameLi = document.createElement("li");
+  const { team: hTeam, points: hPoints } = homeTeam;
+  const { team: aTeam, points: aPoints } = awayTeam;
   const teamNames = `${aTeam} @ ${hTeam}`;
+
   let scoreLine;
   if (aPoints > hPoints) {
     scoreLine = `<b>${aPoints}</b>-${hPoints}`;
   } else {
     scoreLine = `${aPoints}-<b>${hPoints}</b>`;
   }
-  const warriors = hTeam === 'Golden State' ? homeTeam : awayTeam;
-  gameLi.classList.add(warriors.isWinner ? 'win' : 'loss')
 
-  gameLi.innerHTML = `${teamNames} ${scoreLine}`
-  console.log(scoreLine)
+  const warriors = hTeam === "Golden State" ? homeTeam : awayTeam;
+  gameLi.classList.add(warriors.isWinner ? "win" : "loss");
+
+  gameLi.innerHTML = `${teamNames} ${scoreLine}`;
+  console.log(scoreLine);
   ulParent.appendChild(gameLi);
 }
 
-document.body.prepend(ulParent)
+document.body.prepend(ulParent);
