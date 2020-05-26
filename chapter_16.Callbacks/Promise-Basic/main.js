280b577085ee0f64f2eef81aedd8f1cb1c0d42c5
DalatCoder <hieuntctk42@gmail.com>
2020-05-26 21:59:31 +0700

    Pass arguments to resolve and reject

--- chapter_16.Callbacks/Promise-Basic/main.js
+++ chapter_16.Callbacks/Promise-Basic/main.js
@@ -1,42 +1,68 @@
-// const willGetMyDog = new Promise((resolve, reject) => {
-//   setTimeout(() => {
-//     const rand = Math.random();
+// // const willGetMyDog = new Promise((resolve, reject) => {
+// //   setTimeout(() => {
+// //     const rand = Math.random();
 
-//     if (rand < 0.5) {
-//       resolve();
-//     }
+// //     if (rand < 0.5) {
+// //       resolve();
+// //     }
 
-//     reject();
-//   }, 1000);
-// });
+// //     reject();
+// //   }, 1000);
+// // });
 
-const makeDogPromise = () => {
+// const makeDogPromise = () => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       const rand = Math.random();
+
+//       if (rand < 0.5) {
+//         resolve();
+//       }
+
+//       reject();
+//     }, 1000);
+//   });
+// };
+
+// const onSuccess = () => {
+//   const title = document.createElement("h1");
+//   title.innerText = "Yay! I get my dog!";
+//   document.body.appendChild(title);
+// };
+
+// const onFailure = (err) => {
+//   const title = document.createElement("h1");
+//   title.innerText = "Cry Cry!";
+//   document.body.appendChild(title);
+// };
+
+// // willGetMyDog.then(onSuccess);
+// // willGetMyDog.catch(onFailure);
+
+// makeDogPromise().then(onSuccess).catch(onFailure);
+
+const fakeRequest = (url) => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       const rand = Math.random();
-
       if (rand < 0.5) {
-        resolve();
+        resolve({ message: "success", status: "200", data: { data: "2NTH" } });
       }
 
-      reject();
-    }, 1000);
+      reject({ message: "fail", status: "404" });
+    }, 1500);
   });
 };
 
-const onSuccess = () => {
-  const title = document.createElement("h1");
-  title.innerText = "Yay! I get my dog!";
-  document.body.appendChild(title);
-};
-
-const onFailure = (err) => {
-  const title = document.createElement("h1");
-  title.innerText = "Cry Cry!";
-  document.body.appendChild(title);
-};
-
-// willGetMyDog.then(onSuccess);
-// willGetMyDog.catch(onFailure);
-
-makeDogPromise().then(onSuccess).catch(onFailure);
+fakeRequest("2nth.com")
+  .then((response) => {
+    console.log("Request success!");
+    console.log(response);
+  })
+  .catch((response) => {
+    console.log("Request failure!");
+    console.log(response);
+    const { message, status } = response;
+    console.log(`Message: ${message}`);
+    console.log(`Code: ${status}`);
+  });
