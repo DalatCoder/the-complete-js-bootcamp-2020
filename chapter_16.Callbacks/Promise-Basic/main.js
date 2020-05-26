e79970b472ae11e87a6fa4437137371685cb3fca
DalatCoder <hieuntctk42@gmail.com>
2020-05-26 21:32:46 +0700

    Promise Basic

---
+++ chapter_16.Callbacks/Promise-Basic/main.js
@@ -0,0 +1,26 @@
+const willGetMyDog = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    const rand = Math.random();
+
+    if (rand < 0.5) {
+      resolve();
+    }
+
+    reject();
+  }, 1000);
+});
+
+const onSuccess = () => {
+  const title = document.createElement("h1");
+  title.innerText = "Yay! I get my dog!";
+  document.body.appendChild(title);
+};
+
+const onFailure = (err) => {
+  const title = document.createElement("h1");
+  title.innerText = "Cry Cry!";
+  document.body.appendChild(title);
+};
+
+willGetMyDog.then(onSuccess);
+willGetMyDog.catch(onFailure);
