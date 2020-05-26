9e31acb973630b10e636cb36294efdd3dd467747
DalatCoder <hieuntctk42@gmail.com>
2020-05-26 22:52:01 +0700

    Refactoring with promise

--- chapter_16.Callbacks/CallbacksHell/app.js
+++ chapter_16.Callbacks/CallbacksHell/app.js
@@ -1,28 +1,108 @@
 const btn = document.querySelector("button");
 
-btn.style.transition = "all .5s";
-
-// setTimeout(() => {
-//   btn.style.transform = "translateX(100px)";
-
-//   setTimeout(() => {
-//     btn.style.transform = "translateX(200px)";
-
-//     setTimeout(() => {
-//       btn.style.transform = "translateX(300px)";
-//     }, 3000);
-//   }, 2000);
-// }, 1000);
-
-const moveX = (element, amount, delay, callback) => {
+/*
+//This function moves an element "amount" number of pixels after a delay.
+//If the element will stay on screen, we move the element and call the onSuccess callback function
+//If the element will move off screen, we do not move the element and instead call the onFailure callback
+const moveX = (element, amount, delay, onSuccess, onFailure) => {
   setTimeout(() => {
-    element.style.transform = `translateX(${amount}px)`;
-    if (callback) callback();
+    const bodyBoundary = document.body.clientWidth;
+    const elRight = element.getBoundingClientRect().right;
+    const currLeft = element.getBoundingClientRect().left;
+    if (elRight + amount > bodyBoundary) {
+      onFailure();
+    } else {
+      element.style.transform = `translateX(${currLeft + amount}px)`;
+      onSuccess();
+    }
   }, delay);
 };
 
-moveX(btn, 100, 1000, () => {
-  moveX(btn, 200, 1000, () => {
-    moveX(btn, 300, 1000);
+// LOOK AT THIS UGLY MESS!
+moveX(
+  btn,
+  300,
+  1000,
+  () => {
+    //success callback
+    moveX(
+      btn,
+      300,
+      1000,
+      () => {
+        //success callback
+        moveX(
+          btn,
+          300,
+          1000,
+          () => {
+            //success callback
+            moveX(
+              btn,
+              300,
+              1000,
+              () => {
+                //success callback
+                moveX(
+                  btn,
+                  300,
+                  1000,
+                  () => {
+                    //success callback
+                    alert("YOU HAVE A WIDE SCREEN!");
+                  },
+                  () => {
+                    //failure callback
+                    alert("CANNOT MOVE FURTHER!");
+                  }
+                );
+              },
+              () => {
+                //failure callback
+                alert("CANNOT MOVE FURTHER!");
+              }
+            );
+          },
+          () => {
+            //failure callback
+            alert("CANNOT MOVE FURTHER!");
+          }
+        );
+      },
+      () => {
+        //failure callback
+        alert("CANNOT MOVE FURTHER!");
+      }
+    );
+  },
+  () => {
+    //failure callback
+    alert("CANNOT MOVE FURTHER!");
+  }
+);
+
+*/
+
+const moveX = (element, amount, delay) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const bodyBoundary = document.body.clientWidth;
+      const elRight = element.getBoundingClientRect().right;
+      const currLeft = element.getBoundingClientRect().left;
+      if (elRight + amount > bodyBoundary) {
+        reject({ message: "CANNOT MOVE FURTHER!" });
+      } else {
+        element.style.transform = `translateX(${currLeft + amount}px)`;
+        resolve();
+      }
+    }, delay);
   });
-});
+};
+
+moveX(btn, 300, 1000)
+  .then(() => moveX(btn, 300, 1000))
+  .then(() => moveX(btn, 300, 1000))
+  .then(() => moveX(btn, 300, 1000))
+  .then(() => moveX(btn, 300, 1000))
+  .then(() => alert("YOU HAVE A WIDE SCREEN!"))
+  .catch((err) => alert(err.message));
