ce4422bccb057947e6fdf17099826867a9e9f3b7
DalatCoder <hieuntctk42@gmail.com>
2020-05-28 20:21:10 +0700

    Chaining promises axios

--- chapter_17.MakeHTTPRequests/Axios/main.js
+++ chapter_17.MakeHTTPRequests/Axios/main.js
@@ -1,6 +1,36 @@
+/*
 const url = "https://swapi.dev/api/planets";
 
 axios
   .get(url)
   .then((res) => console.log(res.data))
   .catch((err) => console.log("ERROR! ", err));
+*/
+
+axios
+  .get("https://swapi.dev/api/planets")
+  .then(({ data }) => {
+    console.log("The first 10 planets...");
+    for (const planet of data.results) {
+      console.log(planet.name);
+    }
+
+    return axios.get(data.next);
+  })
+  .then(({ data }) => {
+    console.log("The next 10 more planets...");
+    for (const planet of data.results) {
+      console.log(planet.name);
+    }
+
+    return axios.get(data.next);
+  })
+  .then(({ data }) => {
+    console.log("The next 10 more planets...");
+    for (const planet of data.results) {
+      console.log(planet.name);
+    }
+  })
+  .catch((err) => {
+    console.log(`ERROR! ${err}`);
+  });
