dd5ea04f05003bc01b2a6ad16a8edc2f61c04f97
DalatCoder <hieuntctk42@gmail.com>
2020-05-28 09:12:02 +0700

    Chaining XHR request

--- chapter_17.MakeHTTPRequests/UsingXHR/main.js
+++ chapter_17.MakeHTTPRequests/UsingXHR/main.js
@@ -1,8 +1,9 @@
 const firstReq = new XMLHttpRequest();
 
 firstReq.addEventListener("load", function () {
+  console.log("First request work!");
   const res = JSON.parse(this.responseText);
-  console.log(res);
+  // console.log(res);
 
   const planets = res.results.map((data) => {
     const el = document.createElement("p");
@@ -13,6 +14,19 @@ firstReq.addEventListener("load", function () {
   for (let planet of planets) {
     document.body.appendChild(planet);
   }
+
+  filmURL = res.results[0].films[0];
+  const filmRequest = new XMLHttpRequest();
+  filmRequest.addEventListener("load", function () {
+    console.log("Second request work!");
+    const filmData = JSON.parse(this.responseText);
+    console.log(filmData);
+  });
+  filmRequest.addEventListener("error", function (e) {
+    console.log("Error! ", e);
+  });
+  filmRequest.open("GET", filmURL);
+  filmRequest.send();
 });
 firstReq.addEventListener("error", function () {
   console.log("ERROR!");
