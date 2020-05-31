501b4558a731fecdfdf7c03c5fab5fc97c7b60e6
DalatCoder <hieuntctk42@gmail.com>
2020-05-31 16:09:03 +0700

    Class syntax

--- chapter_19.OOP/ColorsSystem/main.js
+++ chapter_19.OOP/ColorsSystem/main.js
@@ -26,11 +26,26 @@ function makeColor(r, g, b) {
 }
 */
 
+/*
+// This is a Constructor Function...
 function Color(r, g, b) {
   this.r = r;
   this.g = g;
   this.b = b;
 }
+
+//If you call it on its own like a regular function...
+Color(35, 60, 190); //undefined
+//It returns undefined. Seems useless!
+
+// *****************
+// THE NEW OPERATOR!
+// *****************
+
+// 1. Creates a blank, plain JavaScript object;
+// 2. Links (sets the constructor of) this object to another object;
+// 3. Passes the newly created object from Step 1 as the this context;
+// 4. Returns this if the function doesn't return its own object.
 
 Color.prototype.rgb = function () {
   const { r, g, b } = this;
@@ -47,8 +62,37 @@ Color.prototype.rgba = function (a = 1.0) {
   return `rgba(${r}, ${g}, ${b}, ${a})`;
 };
 
-const color1 = new Color(255, 0, 0);
-const color2 = new Color(0, 0, 255);
+const color1 = new Color(40, 255, 60);
+color1.hex();
+const color2 = new Color(0, 0, 0);
+color2.hex();
+*/
 
-// Color1.hex and color2.hex is the same object
-console.log(color1.hex === color2.hex);
+class Color {
+  constructor(r, g, b, name) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
+    this.name = name;
+  }
+
+  innerRGB() {
+    const { r, g, b } = this;
+    return `${r}, ${g}, ${b}`;
+  }
+
+  rgb() {
+    return `rgb(${this.innerRGB()})`;
+  }
+
+  hex() {
+    const { r, g, b } = this;
+    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+  }
+
+  rgba(a = 1.0) {
+    return `rgba(${this.innerRGB()}, ${a})`;
+  }
+}
+
+const c1 = new Color(255, 67, 89, "tomato");
