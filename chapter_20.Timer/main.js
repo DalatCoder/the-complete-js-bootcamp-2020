92683013d6d02cc91a59d3e3e3039220aed3eec1
2nth <hieuntctk42@gmail.com>
2020-06-23 16:15:41 +0700

    setInterval

---
+++ chapter_20.Timer/main.js
@@ -0,0 +1,29 @@
+class Timer {
+  constructor(durationInput, startButton, pauseButton) {
+    this.durationInput = durationInput
+    this.startButton = startButton
+    this.pauseButton = pauseButton
+
+    this.startButton.addEventListener('click', this.start)
+    this.pauseButton.addEventListener('click', this.pause)
+  }
+
+  start = () => {
+    this.tick()
+    this.interval = setInterval(this.tick, 1000)
+  }
+
+  pause = () => {
+    clearInterval(this.interval)
+  }
+
+  tick = () => {
+    console.log('Tick')
+  }
+}
+
+const durationInput = document.getElementById('duration')
+const startButton = document.getElementById('start')
+const pauseButton = document.getElementById('pause')
+
+const timer = new Timer(durationInput, startButton, pauseButton)
