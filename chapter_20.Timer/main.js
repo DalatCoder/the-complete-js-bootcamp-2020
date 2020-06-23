e38aef8e04e77720f5f7f62e724d9ca82d24c5ae
2nth <hieuntctk42@gmail.com>
2020-06-23 16:53:35 +0700

    callback pattern

--- chapter_20.Timer/main.js
+++ chapter_20.Timer/main.js
@@ -1,8 +1,12 @@
 class Timer {
-  constructor(durationInput, startButton, pauseButton) {
+  constructor(durationInput, startButton, pauseButton, callbacks) {
     this.durationInput = durationInput
     this.startButton = startButton
     this.pauseButton = pauseButton
+    // callbacks is optional argument
+    if (callbacks) {
+      this.onStart = callbacks.onStart
+    }
 
     this.startButton.addEventListener('click', this.start)
     this.pauseButton.addEventListener('click', this.pause)
@@ -16,6 +20,10 @@ class Timer {
   }
 
   start = () => {
+    if (this.onStart) {
+      this.onStart()
+    }
+
     this.tick()
     this.interval = setInterval(this.tick, 1000)
   }
@@ -38,4 +46,10 @@ const durationInput = document.getElementById('duration')
 const startButton = document.getElementById('start')
 const pauseButton = document.getElementById('pause')
 
-const timer = new Timer(durationInput, startButton, pauseButton)
+const timer = new Timer(durationInput, startButton, pauseButton, {
+  onStart() {
+    console.log('Timer started!')
+  },
+  onTick() {},
+  onComplete() {},
+})
