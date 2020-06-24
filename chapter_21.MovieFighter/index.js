cb36cf7fa0b7cb95b8bf4c1e494af546744ae68e
2nth <hieuntctk42@gmail.com>
2020-06-24 21:07:07 +0700

    Get movies from ombapi

--- chapter_21.MovieFighter/index.js
+++ chapter_21.MovieFighter/index.js
@@ -1,1 +1,14 @@
-console.log('Hi there!');
+const url = "http://www.omdbapi.com/";
+
+const fetchData = async () => {
+  const response = await axios.get(url, {
+    params: {
+      apikey: "e50df26a",
+      s: "avengers",
+    },
+  });
+
+  console.log(response.data);
+};
+
+fetchData();
