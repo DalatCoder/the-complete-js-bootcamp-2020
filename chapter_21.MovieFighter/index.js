34c6c6feffc0e591e339820da3bec2c23ee68ba6
DalatCoder <hieuntctk42@gmail.com>
2020-07-10 18:00:01 +0700

    Parse number

--- chapter_21.MovieFighter/index.js
+++ chapter_21.MovieFighter/index.js
@@ -71,39 +71,59 @@ const runComparison = () => {
 }
 
 const movieTemplate = (movieDetail) => {
+  const {
+    BoxOffice,
+    Poster,
+    Title,
+    Genre,
+    Plot,
+    Awards,
+    Metascore,
+    imdbRating,
+    imdbVotes,
+  } = movieDetail
+
+  // $123,000,000 ==> 123000000
+  const dollars = parseInt(BoxOffice.replace(/\$/g, '').replace(/,/g, ''))
+  const metascore = parseInt(Metascore)
+  const imdbScore = parseFloat(imdbRating)
+  const imdbVotesNumber = parseInt(imdbVotes.replace(/,/g, ''))
+
+  console.log(dollars, metascore, imdbScore, imdbVotesNumber)
+
   return `
     <article class="media">
       <figure class="media-left">
         <p class="image">
-          <img src="${movieDetail.Poster}" />
+          <img src="${Poster}" />
         </p>
       </figure>
       <div class="media-content">
         <div class="content">
-          <h1>${movieDetail.Title}</h1>
-          <h4>${movieDetail.Genre}</h4>
-          <p>${movieDetail.Plot}</p>
+          <h1>${Title}</h1>
+          <h4>${Genre}</h4>
+          <p>${Plot}</p>
         </div>
       </div>
     </article>
     <article class="notification is-primary">
-      <p class="title">${movieDetail.Awards}</p>
+      <p class="title">${Awards}</p>
       <p class="subtitle">Awards</p>
     </article>
-    <article class="notification is-primary">
-      <p class="title">${movieDetail.BoxOffice}</p>
+    <article data-value= class="notification is-primary">
+      <p class="title">${BoxOffice}</p>
       <p class="subtitle">Box Office</p>
     </article>
     <article class="notification is-primary">
-      <p class="title">${movieDetail.Metascore}</p>
+      <p class="title">${Metascore}</p>
       <p class="subtitle">Metascore</p>
     </article>
     <article class="notification is-primary">
-      <p class="title">${movieDetail.imdbRating}</p>
+      <p class="title">${imdbRating}</p>
       <p class="subtitle">IMDB Rating</p>
     </article>
     <article class="notification is-primary">
-      <p class="title">${movieDetail.imdbVotes}</p>
+      <p class="title">${imdbVotes}</p>
       <p class="subtitle">IMDB Votes</p>
     </article>
   `
