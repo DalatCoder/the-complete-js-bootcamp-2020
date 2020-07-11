cafa3c4ef8d81abc0fa4ba2c29bb87d22a7326f7
DalatCoder <hieuntctk42@gmail.com>
2020-07-11 07:05:21 +0700

    Recreate matterjs demo

--- chapter_22.Maze/index.js
+++ chapter_22.Maze/index.js
@@ -1,4 +1,7 @@
-const { Engine, Render, Runner, World, Bodies } = Matter
+const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } = Matter
+
+const width = 800
+const height = 600
 
 const engine = Engine.create()
 const { world } = engine
@@ -6,21 +9,43 @@ const render = Render.create({
   element: document.body,
   engine,
   options: {
-    width: 800,
-    height: 600
+    width,
+    height
   }
 })
 Render.run(render)
 Runner.run(Runner.create(), engine)
 
+World.add(
+  world,
+  MouseConstraint.create(engine, {
+    mouse: Mouse.create(render.canvas)
+  })
+)
+
 // Walls
 const walls = [
-  Bodies.rectangle(400, 0, 800, 40, { isStatic: true }), // top border
-  Bodies.rectangle(400, 600, 800, 40, { isStatic: true }), // bottom border
-  Bodies.rectangle(0, 300, 40, 600, { isStatic: true }), // left border
-  Bodies.rectangle(800, 300, 40, 600, { isStatic: true }) // right border
+  Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }), // top border
+  Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }), // bottom border
+  Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }), // left border
+  Bodies.rectangle(width, height / 2, 40, height, { isStatic: true }) // right border
 ]
 World.add(world, walls)
 
 const shape = Bodies.rectangle(200, 200, 50, 50)
 World.add(world, shape)
+
+// Random shapes
+for (let i = 0; i < 50; i++) {
+  if (Math.random() > 0.5) {
+    World.add(
+      world,
+      Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50)
+    )
+  } else {
+    World.add(
+      world,
+      Bodies.circle(Math.random() * width, Math.random() * height, 35)
+    )
+  }
+}
