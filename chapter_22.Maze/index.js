919068d0c9fe7a787566d1069a587ee5d9151667
DalatCoder <hieuntctk42@gmail.com>
2020-07-11 14:26:06 +0700

    Complete logic section

--- chapter_22.Maze/index.js
+++ chapter_22.Maze/index.js
@@ -1,5 +1,6 @@
 const { Engine, Render, Runner, World, Bodies } = Matter
 
+const cells = 3
 const width = 600
 const height = 600
 const edgeWeight = 40
@@ -27,18 +28,63 @@ const walls = [
 World.add(world, walls)
 
 // Maze generation || Generate 2 dimensional array with defaul value set to false
-const grid = Array(3)
-  .fill(null)
-  .map(() => Array(3).fill(false))
+const grid = generateGrid(cells, cells)
+const verticals = generateGrid(cells, cells - 1)
+const horizontals = generateGrid(cells - 1, cells)
 
-const verticals = Array(3)
-  .fill(null)
-  .map(() => Array(2).fill(false))
+const startRow = Math.floor(Math.random() * cells)
+const startColumn = Math.floor(Math.random() * cells)
 
-const horizontals = Array(2)
-  .fill(null)
-  .map(() => Array(3).fill(false))
+const stepThroughCell = (row, column) => {
+  // If i have visited the cell at [row, column], then return
+  if (grid[row][column]) {
+    return
+  }
 
-console.log(grid)
-console.log(verticals)
-console.log(horizontals)
+  // Mark this cell as being visited
+  grid[row][column] = true
+
+  // Assemble randomly-ordered list of neighbors
+  const neighbors = suffle([
+    [row - 1, column, 'up'], // top
+    [row + 1, column, 'down'], // bottom
+    [row, column - 1, 'left'], // left
+    [row, column + 1, 'right'] // right
+  ])
+
+  // For each neighbor
+  for (const neighbor of neighbors) {
+    const [nextRow, nextColumn, direction] = neighbor
+
+    // See if that neighbor is out of bounds
+    if (
+      nextRow < 0 ||
+      nextRow >= cells ||
+      nextColumn < 0 ||
+      nextColumn >= cells
+    ) {
+      continue
+    }
+
+    // If we have visited that neighbor, continue to next neighbor
+    if (grid[nextRow][nextColumn]) {
+      continue
+    }
+
+    // Remove a wall from either horizontals or verticals
+    if (direction === 'left') {
+      verticals[row][column - 1] = true
+    } else if (direction === 'right') {
+      verticals[row][column] = true
+    } else if (direction === 'up') {
+      horizontals[row - 1][column] = true
+    } else if (direction === 'down') {
+      horizontals[row][column] = true
+    }
+
+    // Visit that next cell
+    stepThroughCell(nextRow, nextColumn)
+  }
+}
+
+stepThroughCell(startRow, startColumn)
--- chapter_22.Maze/utils.js
+++ chapter_22.Maze/utils.js
@@ -8,3 +8,23 @@ const generateGrid = (row, column) => {
     .fill(null)
     .map(() => Array(column).fill(false))
 }
+
+/**
+ * Function to sort every element in the given array randomly
+ * @param {Array} arr
+ */
+const suffle = (arr) => {
+  let counter = arr.length
+
+  while (counter > 0) {
+    const index = Math.floor(Math.random() * counter)
+    counter--
+
+    // swap
+    const temp = arr[counter]
+    arr[counter] = arr[index]
+    arr[index] = temp
+  }
+
+  return arr
+}
