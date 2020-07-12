754d6dbb6c5b9a5633f241379b721f8f85de7ca4
DalatCoder <hieuntctk42@gmail.com>
2020-07-12 09:47:45 +0700

    Encrypt message from ascii to base64

--- chapter_23.Message/index.js
+++ chapter_23.Message/index.js
@@ -2,6 +2,8 @@ document.querySelector('form').addEventListener('submit', (event) => {
   // Prevent browser from auto refresh webpage
   event.preventDefault()
 
-  const input = document.querySelector('#input')
-  console.log(input.value)
+  const input = document.querySelector('#message-input')
+  const encrypted = btoa(input.value)
+
+  document.querySelector('#link-input').value = encrypted
 })
