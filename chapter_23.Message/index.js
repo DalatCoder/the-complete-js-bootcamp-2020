010c683e975cc4a7f9829b0f1af8f1cb49dab33c
DalatCoder <hieuntctk42@gmail.com>
2020-07-12 10:10:28 +0700

    Create link form

--- chapter_23.Message/index.js
+++ chapter_23.Message/index.js
@@ -2,8 +2,15 @@ document.querySelector('form').addEventListener('submit', (event) => {
   // Prevent browser from auto refresh webpage
   event.preventDefault()
 
+  // Hide message form
+  document.querySelector('#message-form').classList.add('hide')
+  // Make link form appear
+  document.querySelector('#link-form').classList.remove('hide')
+
   const input = document.querySelector('#message-input')
   const encrypted = btoa(input.value)
 
-  document.querySelector('#link-input').value = encrypted
+  const linkInput = document.querySelector('#link-input')
+  linkInput.value = `${window.location}#${encrypted}`
+  linkInput.select()
 })
